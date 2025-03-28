d1e9e0b43aad6721ab21c86a2dc33104a30272aa
yen <yengit@yenmail.party>
2025-03-28 12:35:03 +0100

    Add nix flake with a dev shell to build Mixxx

---
+++ tools/flake.nix
@@ -0,0 +1,122 @@
+{
+  inputs = {
+    utils.url = "github:numtide/flake-utils";
+    nixpkgs.url = "github:NixOS/nixpkgs/release-24.11";
+  };
+  outputs = { self, nixpkgs, utils }: utils.lib.eachDefaultSystem (system:
+    let
+      pkgs = nixpkgs.legacyPackages.${system};
+      qt6Env = with pkgs.qt6; env "qt-custom-${qtbase.version}"
+        [
+          qt5compat
+          qtshadertools
+          qtsvg
+          qtdeclarative
+
+
+          #qt3d
+          #qtcharts
+          #qtconnectivity
+          #qtdatavis3d
+          #qtdeclarative
+          #qtdoc
+          #qtgraphs
+          #qtgrpc
+          #qthttpserver
+          #qtimageformats
+          #qtlanguageserver
+          #qtlocation
+          #qtlottie
+          #qtmultimedia
+          #qtmqtt
+          #qtnetworkauth
+          #qtpositioning
+          #qtsensors
+          #qtserialbus
+          #qtserialport
+          #qtspeech
+          #qtquick3d
+          #qtquick3dphysics
+          #qtquickeffectmaker
+          #qtquicktimeline
+          #qtremoteobjects
+          #qtscxml
+          #qttools
+          #qttranslations
+          #qtvirtualkeyboard
+          #qtwebchannel
+          #qtwebengine
+          #qtwebsockets
+          #qtwebview
+        ];
+    in
+    {
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          git
+
+          # Building Mixxx
+          qt6Env
+          #kdePackages.qtkeychain
+          cmake
+          chromaprint
+          glib
+          libebur128
+          fftw
+          flac
+          lame
+          libogg
+          libvorbis
+          portaudio
+          portmidi
+          protobuf
+          rubberband
+          libsndfile
+          soundtouch
+          taglib
+          upower
+          openssl
+          microsoft-gsl
+          kdePackages.qtkeychain
+          hidapi
+          wavpack
+          libid3tag
+          libusb1
+          libmad
+          libopus
+          opusfile
+          libshout
+          lilv
+          libxkbcommon
+          sqlite
+          gtest
+          clang-tools
+          mp4v2
+          vulkan-loader
+          xorg.libX11
+          ffmpeg
+          libmodplug
+          vamp-plugin-sdk
+          ccache
+          libGLU
+          pcre
+          libselinux
+          utillinux
+          libdjinterop
+          libkeyfinder
+          cups
+          lv2
+
+          # Git pre-commits
+          pre-commit
+          nodejs
+          rustup
+        ];
+        shellHook = ''
+          pre-commit install
+          pre-commit install -t pre-push
+        '';
+      };
+    }
+  );
+}
