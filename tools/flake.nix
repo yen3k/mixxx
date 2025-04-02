167a2201cdb096a65ecd37d7c7cd2e882bb85a0e
yen <yengit@yenmail.party>
2025-04-02 21:42:51 +0200

    :)))

--- tools/flake.nix
+++ tools/flake.nix
@@ -78,7 +78,7 @@
           pre-commit install
           pre-commit install -t pre-push
           # Needed for clang-format pre-commit because it downloads and executes its own clang-format elf-binary
-          export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib}/lib/"
+          export LD_LIBRARY_PATH="${pkgs.stdenv.cc.cc.lib}/lib/:$LD_LIBRARY_PATH"
         '';
       };
     }
